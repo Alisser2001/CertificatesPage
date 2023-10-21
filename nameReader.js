5fcc667ab56632cf2010e2bc2f6761e2675f0bc8
unknown <estiven.carmona@udea.edu.co>
2023-10-20 22:01:07 -0500

    Primera versión terminada

--- nameReader.js
+++ nameReader.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const directoryPath = './public/assets/';
 const outputFile = 'certificatesInfo.json';
+const imageFormat = 'jpg';
 
 if (fs.existsSync(outputFile)) {
     fs.unlinkSync(outputFile);
@@ -15,7 +16,11 @@ fs.readdir(directoryPath, (err, files) => {
     }
 
     const pdfFiles = files.filter(file => path.extname(file) === '.pdf');
-    const jsonArray = pdfFiles.map(file => ({ name: file, relevance: "" }));
+    const jsonArray = pdfFiles.map(file => {
+        const fileName = path.basename(file, '.pdf'); // Obtiene el nombre sin la extensión
+        const imgName = `${fileName}.${imageFormat}`;
+        return { name: fileName, img: imgName };
+    });
 
     const jsonContent = JSON.stringify(jsonArray, null, 2);
 
