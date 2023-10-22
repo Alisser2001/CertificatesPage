25684e7c6fefb507c88f9c5701041023acf2f2ae
unknown <estiven.carmona@udea.edu.co>
2023-10-22 11:23:12 -0500

    Correción al script pdf2img

--- scripts/pdf2image.js
+++ scripts/pdf2image.js
@@ -1,20 +1,44 @@
+/*
+OS requirements to use the script:
+
+- Node >= 14.x
+- ImageMagick
+- GraphicsMagick
+- GhostScript
+
+Python libraries required:
+
+- pdf2pic
+- fs-extra
+*/
+
 const { fromPath } = require('pdf2pic');
 const fs = require('fs');
 const path = require('path');
-const fsExtra = require('fs-extra'); 
+const fsExtra = require('fs-extra');
 
-const pdfPath = './public/assets';
+const directoryPath = './public/assets/';
 const outputFolder = './public/miniatures';
+const outputFile = './certificatesInfo.json';
 
+if (fs.existsSync(outputFile)) {
+    fs.unlinkSync(outputFile);
+}
 fsExtra.emptyDirSync(outputFolder);
 
-fs.readdir(pdfPath, (err, files) => {
+fs.readdir(directoryPath, (err, files) => {
     if (err) {
         console.error('Error al leer el directorio:', err);
         return;
     }
+
     const pdfFiles = files.filter(file => path.extname(file) === '.pdf');
-    
+    const jsonArray = pdfFiles.map(file => {
+        const fileName = path.basename(file, '.pdf');
+        return { name: fileName };
+    });
+    const jsonContent = JSON.stringify(jsonArray, null, 2);
+    fs.writeFileSync(outputFile, jsonContent);
     pdfFiles.forEach(file => {
         const fileName = path.basename(file, '.pdf');
         const options = {
@@ -25,7 +49,7 @@ fs.readdir(pdfPath, (err, files) => {
             width: 600,
             height: 400
         };
-        const convert = fromPath(`${pdfPath}/${file}`, options);
+        const convert = fromPath(`${directoryPath}/${file}`, options);
 
         convert(1)
             .catch((error) => {
