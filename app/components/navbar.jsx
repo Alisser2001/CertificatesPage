d0eb0cf01bccae9e75fcdc3568540b47623d4829
unknown <estiven.carmona@udea.edu.co>
2023-10-25 11:37:32 -0500

    Primera vista responsive

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -6,15 +6,15 @@ export const NavBar = () => {
         <nav className={styles.container}>
             <h1 className={styles.title}><span>&lt;</span>ALIDEV<span>/&gt;</span></h1>
             <ul className={styles.options}>
-                <li className={styles.aboutLink}>
+                <li className={styles.certLink}>
                     <a href="/" className={styles.link}>
-                        <img src="/certificate.png" className={styles.aboutImg} alt="toCertificates"/>
+                        <img src="/certificate.png" className={styles.certImg} alt="toCertificates"/>
                         <p className={styles.linkTitle}>Certificates</p>
                     </a>
                 </li>
-                <li className={styles.projectsLink}>
+                <li className={styles.portLink}>
                     <a href="https://portfolio-alisser2001.vercel.app/" target="_blank" className={styles.link}>
-                        <img src="/portfolio.png" className={styles.projectsImg} alt="toPortfolio"/>
+                        <img src="/portfolio.png" className={styles.portImg} alt="toPortfolio"/>
                         <p className={styles.linkTitle}>Portfolio</p>
                     </a>
                 </li>
