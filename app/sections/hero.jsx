74e8be49f5c8541cf1159c3899be17f02e90eac8
Alisser2001 <estiven.carmona@udea.edu.co>
2024-07-26 13:38:53 -0500

    Nuevo diseño - Correccion visualizacion de pdf

--- app/sections/hero.jsx
+++ app/sections/hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
             <p className='flex flex-row justify-start items-center w-full h-auto text-8xl font-black mb-5 text-left animate-fade-bottom-y-2 opacity-0' style={{animationDelay: ".8s"}}>Estiven Muñoz</p>
             <p className='flex flex-row justify-start items-center w-full h-auto text-7xl font-black mb-5 text-left animate-fade-bottom-y-3 opacity-0' style={{animationDelay: ".8s"}}>I build things for the web.</p>
             <p className='flex flex-row justify-start items-center w-3/4 h-auto text-xl font-semibold mt-5 text-left leading-10 animate-fade-bottom-y-4 opacity-0' style={{animationDelay: ".8s"}}>
-                I'm Estiven Muñoz, a full stack web developer with experience working in languages like JavaScript, TypeScript, C#, Python and Java. Mainly focused on back-end development, I've participated in different projects developing CRUD functionalities for web servers and creating RESTful APIs.
+                I am Estiven Muñoz, a full stack web developer with experience working in languages like JavaScript, TypeScript, C#, Python and Java. Mainly focused on back-end development, I have participated in different projects developing CRUD functionalities for web servers and creating RESTful APIs.
             </p>
             <div className="w-full h-auto flex flex-row justify-start items-center animate-fade-bottom-y-5 opacity-0 mt-6" style={{ animationDelay: ".8s" }}>
                 <a href="https://portfolio-alisser2001s-projects.vercel.app/" target="_blank" className="bg-[#161616] relative z-30 cursor-pointer flex justify-center items-center w-40 h-12 rounded-lg border-2 border-[#2E28A0] font-bold text-xl text-white transform transition duration-300 hover:-translate-x-1 hover:-translate-y-1">
