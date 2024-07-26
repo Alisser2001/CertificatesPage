52296f486d1166122cdc5a437f6fd515998617bd
Alisser2001 <estiven.carmona@udea.edu.co>
2024-07-26 13:29:44 -0500

    Nuevo diseño - Correccion visualizacion de pdf

--- app/viewpdf/[slug]/page.jsx
+++ app/viewpdf/[slug]/page.jsx
@@ -4,13 +4,14 @@ import Iframe from 'react-iframe';
 
 export default function pdf({ params }) {
     return (
-        <div className="container">
-            <Iframe
-                url={`/assets/${params.slug}.pdf`}
-                id="pdf-iframe"
-                width="800px"
-                height="600px"
-            />
+        <div className="flex flex-col p-0 m-0 justify-center items-center w-screen h-auto">
+            <section className="flex flex-col w-1/2 h-screen justify-center items-center mt-12">
+                <Iframe
+                    url={`/assets/${params.slug}.pdf`}
+                    id="pdf-iframe"
+                    className="w-full h-3/4"
+                />
+            </section>
         </div>
     )
 }
