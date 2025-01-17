5732a77e9eee50b5abe7d2a3a5aea198fa3c048c
Alisser2001 <estiven.carmona@udea.edu.co>
2025-01-17 18:38:29 -0500

    Privera version del nuevo diseno

--- app/components/showButton.jsx
+++ app/components/showButton.jsx
@@ -12,10 +12,10 @@ export default function ShowButton({ certificatesInfo, actualNumItems, setActual
     }
     return (
         <div className="w-full h-auto flex flex-row justify-end items-center animate-fade-bottom-y-5 opacity-0 mt-16" style={{ animationDelay: ".8s" }}>
-            <span href="#" onClick={handleActualNumItems} className="left-40 bg-[#161616] relative z-30 cursor-pointer flex justify-center items-center w-40 h-12 rounded-lg border-2 border-[#2E28A0] font-bold text-xl text-white transform transition duration-300 hover:-translate-x-1 hover:-translate-y-1">
+            <span href="#" onClick={handleActualNumItems} className="bg-[#161616] z-30 cursor-pointer flex justify-center items-center w-40 h-12 rounded-lg border-2 border-[#2E28A0] font-bold text-xl text-white transform transition duration-300 hover:-translate-x-1 hover:-translate-y-1">
                 {actualNumItems >= certificatesInfo.length ? "Show less" : "Show more"}
             </span>
-            <div className="w-40 h-12 bg-[#2E28A0] z-0 relative rounded-lg"></div>
+            <div className="w-40 h-12 bg-[#2E28A0] z-0 absolute rounded-lg"></div>
         </div>
     )
 }
